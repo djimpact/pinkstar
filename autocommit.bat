9a479927e3816c0a52c5ad2aa0baf87527ad2018
djimpact <82122833+djimpact@users.noreply.github.com>
2023-06-21 14:20:41 +0200

    Create autocommit.bat

---
+++ autocommit.bat
@@ -0,0 +1,6 @@
+@echo off
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+git add .
+git commit -m "Autocommit %mydate%_%mytime%'
+git push origin main
